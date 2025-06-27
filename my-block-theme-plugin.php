d157ffe6ad18f0c73941cc883a933736553740c3
kurudrive <kurudrive@gmail.com>
2025-06-28 08:38:17 +0900

    lint

--- my-block-theme-plugin.php
+++ my-block-theme-plugin.php
@@ -8,19 +8,28 @@ License: GPL2 or later
 */
 
 /**
- * カスタムフィールドの値を表示するショートコードの関数
- * ショートコードの使用例: [get-custom-field name="price"]
+ * カスタムフィールドの値を表示するショートコード。
+ * 使用例: [get-custom-field name="price"]
+ *
+ * @param array $atts ショートコード属性。
+ * @return string カスタムフィールド値または空文字。
  */
-function mbtp_get_custom_field_shortcode($atts) {
-    global $post;
-    if (!isset($post->ID)) return '';
-    $atts = shortcode_atts([
-        'name' => '',
-    ], $atts);
-    if (empty($atts['name'])) return '';
-    $value = get_post_meta($post->ID, $atts['name'], true);
-    if ($value === '') return '';
-    return esc_html($value);
+function mbtp_get_custom_field_shortcode( $atts ) {
+	global $post;
+	if ( ! isset( $post->ID ) ) {
+		return '';
+	}
+	$atts = shortcode_atts( [
+		'name' => '',
+	], $atts );
+	if ( empty( $atts['name'] ) ) {
+		return '';
+	}
+	// カスタムフィールドの値を取得
+	$value = get_post_meta( $post->ID, $atts['name'], true );
+	if ( $value === '' ) {
+		return '';
+	}
+	return esc_html( $value );
 }
-// ショートコードの名前, ショートコードで実行する関数名
-add_shortcode('get-custom-field', 'mbtp_get_custom_field_shortcode');
+add_shortcode( 'get-custom-field', 'mbtp_get_custom_field_shortcode' );
