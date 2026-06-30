783d07cf171ebdffafd52b61144478717080d4c1
kurudrive <kurudrive@gmail.com>
2026-06-30 16:15:50 +0900

    余計なサンプルコード削除

--- my-block-theme-plugin.php
+++ my-block-theme-plugin.php
@@ -7,29 +7,4 @@ Author:
 License: GPL2 or later
 */
 
-/**
- * カスタムフィールドの値を表示するショートコード。
- * 使用例: [get-custom-field name="price"]
- *
- * @param array $atts ショートコード属性。
- * @return string カスタムフィールド値または空文字。
- */
-function mbtp_get_custom_field_shortcode( $atts ) {
-	global $post;
-	if ( ! isset( $post->ID ) ) {
-		return '';
-	}
-	$atts = shortcode_atts( [
-		'name' => '',
-	], $atts );
-	if ( empty( $atts['name'] ) ) {
-		return '';
-	}
-	// カスタムフィールドの値を取得
-	$value = get_post_meta( $post->ID, $atts['name'], true );
-	if ( $value === '' ) {
-		return '';
-	}
-	return esc_html( $value );
-}
-add_shortcode( 'get-custom-field', 'mbtp_get_custom_field_shortcode' );
+// カスタマイズ要の PHP を適当に追加してください
